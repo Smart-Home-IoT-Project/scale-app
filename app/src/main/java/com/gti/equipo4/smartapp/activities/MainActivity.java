b8163bf3369c43933955dca67ddca44d0109f99f
Joan Ciprià <joancipria@gmail.com>
2018-11-06 22:19:51 +0100

    Limpieza de código

--- app/src/main/java/com/gti/equipo4/smartapp/activities/MainActivity.java
+++ app/src/main/java/com/gti/equipo4/smartapp/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.gti.equipo4.smartapp.activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -12,19 +13,19 @@ import androidx.fragment.app.FragmentTransaction;
 import android.preference.PreferenceManager;
 import android.view.View;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.tabs.TabLayout;
 import com.gti.equipo4.smartapp.R;
+import com.gti.equipo4.smartapp.adapters.Weights;
+import com.gti.equipo4.smartapp.adapters.WeigthsFirestoreUI;
 import com.gti.equipo4.smartapp.fragments.menu.scale;
 import com.gti.equipo4.smartapp.fragments.menu.home;
 import com.gti.equipo4.smartapp.fragments.menu.sensors;
-import com.gti.equipo4.smartapp.fragments.scale.SampleFragmentPagerAdapter;
 
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,11 +33,16 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     public boolean isFirstStart;
+    private RecyclerView recyclerView;
+    public Weights adaptador;
+    public static WeigthsFirestoreUI adaptador2;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //Declare a new thread to do a preference check
         Thread t = new Thread(new Runnable() {
@@ -168,4 +174,9 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /*@Override public void onDestroy() {
+        super.onDestroy();
+        adaptador2.stopListening();
+    }*/
 }
