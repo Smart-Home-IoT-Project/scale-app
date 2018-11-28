52d50fb94dd395f3429f183fc7442245b80c733c
Juan Canet <info@juancanet.es>
2018-11-28 10:18:25 +0100

    Sistema de notificaciones

---
+++ app/src/main/java/com/gti/equipo4/smartapp/activities/Notifications.java
@@ -0,0 +1,43 @@
+package com.gti.equipo4.smartapp.activities;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+
+import com.gti.equipo4.smartapp.R;
+
+import androidx.core.app.NotificationCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+public class Notifications {
+    private NotificationManager notificationManager;
+    static final String CANAL_ID = "mi_canal";
+    static final int NOTIFICACION_ID = 1;
+
+    @Override
+    public int onStartCommand(Intent intenc, int flags, int idArranque) {
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CANAL_ID, "Mis Notificaciones",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Descripcion del canal");
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificacion =
+                new NotificationCompat.Builder(MainActivity.this, CANAL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Título")
+                        .setContentText("Texto de la notificación.");
+        PendingIntent intencionPendiente = PendingIntent.getActivity(
+                this, 0, new Intent(this, MainActivity.class), 0);
+        notificacion.setContentIntent(intencionPendiente);
+        notificationManager.notify(NOTIFICACION_ID, notificacion.build());
+    }
+    @Override public void onDestroy() {
+        notificationManager.cancel(NOTIFICACION_ID);
+    }
+}
